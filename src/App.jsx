ea3be46b1b291b878247ca01d5f8a579dfb2817e
gencive5 <vic.segen@gmail.com>
2024-11-07 18:13:54 +0100

    gradient background ok
    removed 2nd column

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Carousel from './components/FirstCarousel.jsx';
 import logo from './assets/images/logo.svg';
 import { Gradient } from './components/Gradient.js';
@@ -24,31 +21,21 @@ function App() {
   };
 
   return (
-    <Container fluid className="custom-container">
-      <Row className="no-gutters">
-        <Col sm={12} md={8} className="col-first">
-          
-        <canvas id="gradient-canvas" ref={canvasRef}></canvas>
-
-          <div className="main-zone">
-          
-            <div className="logo-container">
-              <img src={logo} alt="Logo" className="logo" />
-            </div>
-            <div className="carousel-container">
-              <Carousel activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
-            </div>
-            <div className="button-container">
-              <YesButton handleNext={handleNext} />
-              <NoButton handleNext={handleNext} />
-            </div>
-          </div>
-        </Col>
-        <Col sm={12} md={4} className="col-second">
-          {/* Leave the second column empty for now or add other content */}
-        </Col>
-      </Row>
-    </Container>
+    <div className="fullscreen-container">
+      <canvas id="gradient-canvas" ref={canvasRef}></canvas>
+      <div className="main-zone">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div className="carousel-container">
+          <Carousel activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+        </div>
+        <div className="button-container">
+          <YesButton handleNext={handleNext} />
+          <NoButton handleNext={handleNext} />
+        </div>
+      </div>
+    </div>
   );
 }
 
