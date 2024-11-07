2c9444302d5a392f0682c1803fcbf5340b489093
gencive5 <vic.segen@gmail.com>
2024-11-07 16:52:38 +0100

    gradient background ok,
    need to remove grey right strip

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Container from 'react-bootstrap/Container';
@@ -12,100 +12,42 @@ import NoButton from './components/No.jsx';
 
 function App() {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [canScroll, setCanScroll] = useState(true);
-  
   const canvasRef = useRef(null);
 
-
-  // Initialize gradient
   useEffect(() => {
     const gradient = new Gradient();
     gradient.initGradient('#gradient-canvas');
   }, []);
 
-  // Handle scroll and keyboard events for slide changes
-  useEffect(() => {
-    if (window.innerWidth > 768) {
-      let scrollTimeout;
-
-      const handleScroll = (event) => {
-        if (!canScroll) return;
-
-        clearTimeout(scrollTimeout);
-        setCanScroll(false);
-
-        const delta = Math.sign(event.deltaY);
-        const newIndex = (activeIndex + delta + 4) % 4;
-
-        setActiveIndex(newIndex);
-
-        scrollTimeout = setTimeout(() => {
-          setCanScroll(true);
-        }, 500);
-      };
-
-      const handleKeydown = (event) => {
-        if (!canScroll) return;
-
-        if (event.key === 'ArrowUp') {
-          handlePrevious();
-        } else if (event.key === 'ArrowDown') {
-          handleNext();
-        }
-      };
-
-      window.addEventListener('wheel', handleScroll);
-      window.addEventListener('keydown', handleKeydown);
-
-      return () => {
-        window.removeEventListener('wheel', handleScroll);
-        window.removeEventListener('keydown', handleKeydown);
-      };
-    }
-  }, [activeIndex, canScroll]);
-
-  const handlePrevious = () => {
-    const newActiveIndex = activeIndex === 0 ? 3 : activeIndex - 1;
-    setActiveIndex(newActiveIndex);
-  };
-
   const handleNext = () => {
-    const newActiveIndex = (activeIndex + 1) % 4;
-    setActiveIndex(newActiveIndex);
+    setActiveIndex((prevIndex) => (prevIndex + 1) % 4);
   };
-
- 
-
 
   return (
     <Container fluid className="custom-container">
       <Row className="no-gutters">
         <Col sm={12} md={8} className="col-first">
+          
+        <canvas id="gradient-canvas" ref={canvasRef}></canvas>
+
           <div className="main-zone">
-            
-              <div className="logo-container">
-                <img src={logo} alt="Logo" className="logo" />
-              </div>
-               
+          
+            <div className="logo-container">
+              <img src={logo} alt="Logo" className="logo" />
+            </div>
             <div className="carousel-container">
               <Carousel activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
-              
             </div>
-             
-            
-              <div className="button-container">
-                <YesButton handleNext={handleNext} />
-                <NoButton handleNext={handleNext} />
-              </div>
-           </div>
-
-            
+            <div className="button-container">
+              <YesButton handleNext={handleNext} />
+              <NoButton handleNext={handleNext} />
+            </div>
+          </div>
         </Col>
         <Col sm={12} md={4} className="col-second">
-          <canvas id="gradient-canvas" ref={canvasRef}></canvas>
+          {/* Leave the second column empty for now or add other content */}
         </Col>
       </Row>
-      
     </Container>
   );
 }
