dc49c2ca3932b312f0a3dc1e3e3fe47ca7e56bc5
gencive5 <vic.segen@gmail.com>
2024-11-26 23:32:51 +0100

    console log to see if buttons click

--- src/components/No.jsx
+++ src/components/No.jsx
@@ -3,6 +3,7 @@ import noIcon from '../assets/images/crossb.png';
 
 
 const NoButton = ({ handleNext }) => {
+  console.log("Button clicked!"); 
   return (
     <button className="no-button" onClick={handleNext}>
       <img src={noIcon} alt="No Icon" className="no-icon" />
