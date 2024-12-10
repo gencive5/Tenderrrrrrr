bc70b1ac0bf96331ee4fce83d18768ec223c714f
gencive5 <vic.segen@gmail.com>
2024-12-10 23:28:21 +0100

    yes&no buttons fills when clicked
    needs twealing and no has to be fixed

--- src/components/No.jsx
+++ src/components/No.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import noIcon from '../assets/images/stop.svg';
-
+import React, { useState } from 'react';
 
 const NoButton = ({ handleNext }) => {
-  console.log("Button clicked!"); 
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    handleNext();
+
+    // Reset the clicked state after a short delay (e.g., 500ms)
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 500);
+  };
+
   return (
-    <button className="no-button" onClick={handleNext}>
-      <img src={noIcon} alt="No Icon" className="no-icon" />
+    <button className="no-button" onClick={handleClick}>
+      <svg
+        className={`no-icon ${isClicked ? 'clicked' : ''}`}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 194.5 194.5"
+        width="40"
+        height="40"
+      >
+        <circle className="no-path" cx="97.25" cy="97.25" r="95.25" />
+        <rect className="no-path" x="55.65" y="83.38" width="106.7" height="36.17" />
+      </svg>
     </button>
   );
 };
