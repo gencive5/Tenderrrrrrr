f35bd1015318574b7708ffd54b0ab8a466a11fad
Vic Segen <vic.segen@gmail.com>
2026-05-04 12:32:53 +0200

    new buttons test (png) 🔘

--- src/components/No.jsx
+++ src/components/No.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import cross from '../components/cross.png';
 
 const NoButton = ({ handleNext }) => {
   const [isClicked, setIsClicked] = useState(false);
@@ -15,16 +16,7 @@ const NoButton = ({ handleNext }) => {
 
   return (
     <button className="no-button" onClick={handleClick}>
-      <svg
-        className={`no-icon ${isClicked ? 'clicked' : ''}`}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 194.5 194.5"
-        width="40"
-        height="40"
-      >
-        <circle className="no-path" cx="97.25" cy="97.25" r="95.25" />
-        <rect className="no-path" x="55.65" y="83.38" width="106.7" height="36.17" />
-      </svg>
+       <img src={cross} className="no-icon" alt="no" />
     </button>
   );
 };
